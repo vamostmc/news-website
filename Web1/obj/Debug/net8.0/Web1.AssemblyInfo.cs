9a316c4903aa99b48c4935f53bf067d1ccd2aef4
vamostmc <trinhminhchienx@gmail.com>
2024-11-04 23:46:45 +0700

    new 11:46 new1

--- Web1/obj/Debug/net8.0/Web1.AssemblyInfo.cs
+++ Web1/obj/Debug/net8.0/Web1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f5eddbc0ad1acc4beaeef9c17b11ff7a979e5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+430553699db37b779a6c559b784b36c42a37251b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
